547cb1ff69748802d05782045870f4fcfc0e95e8
anjanettecastillo15 <adventureswithanjanette@gmail.com>
2021-01-18 22:38:16 +0800

    anja commit

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+    $dbhost = "localhost: 3307";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "login_db";
+    
+
+    if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
+    {
+
+        die("failed to connect!");
+    }
+?>
