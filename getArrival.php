69661d0da4f66b5f035606abc88cbbcb6ddc6bd6
anjanettecastillo15 <adventureswithanjanette@gmail.com>
2021-01-23 22:24:10 +0800

    jan 23 anja commit

---
+++ getArrival.php
@@ -0,0 +1,20 @@
+<?php
+    require_once("dbcontroller.php");
+
+    $db_handle = new DBController();
+
+    if(!empty($_POST["bound_id"])){
+        $query = "SELECT * FROM arrival WHERE boundID = " . $_POST["bound_id"] . " order by name asc";
+        $results = $db_handle->runQuery($query);
+        ?>
+
+<option value disabled selected> Select Arrival Station</option>
+<?php
+    foreach ($results as $arrival) {
+        ?>
+<option value="<?php echo $arrival["id"]; ?>"><?php echo $arrival["arrival_station"];?></option>
+<?php
+    }
+}
+?>
+
--- index.php
+++ index.php
@@ -12,89 +12,125 @@ $user_data = check_login($con);
 if($_SERVER['REQUEST_METHOD'] == "POST"){
 
     //$passengers = $_POST['passengers'];
+    $bound = $_POST['bound'];
     $passengertype = $_POST['passengertype'];
     $departure = $_POST['departure'];
     $arrival = $_POST['arrival'];
 }
 ?>
 
+<?php
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+$query = "SELECT * FROM bound";
+$results = $db_handle->runQuery($query);
+?>
+
 
 <html>
 <head>
     <title>PNR Online Ticketing | Home</title>
-    
+</head>  
+<style type="text/css">
+    body{width: 800px;
+        font-family: calibri;
+        padding: 0;
+        margin: 0 auto;
+        }
+    .frm{
+        border:1px solid #7ddaff;
+        background-color: #b4c8d0;
+        margin: 0px auto;
+        padding: 40px;
+        border-radius: 4px;
+    }
+    .InputBox{
+        padding: 10px;
+        border:#bdbdbd 1px solid;
+        border-radius: 4px;
+        background-color: #FFF;
+        width: 50%;
+    }
+    .row{
+        padding-bottom: 15px;
+        padding-left: 150px;
+    }
+</style>
+<script src="jquery.main.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+    function getDeparture(val){
+        $.ajax({
+            type: "POST",
+            url: "getDeparture.php",
+            data: 'bound_id='+val,
+            success:function(data){
+                $("#departure-list").html(data);
+                getArrival();
+            }
+        });
+    }
+    function getArrival(val){
+        $.ajax({
+            type: "POST",
+            url: "getArrival.php",
+            data: 'departure_id='+val,
+            success:function(data){
+                $("#arrival-list").html(data);
+                getTime();
+            }
+        });
+    }
+    function getTime(val){
+        $.ajax({
+            type: "POST",
+            url: "getTime.php",
+            data: 'arrival_id='+val,
+            success:function(data){
+                $("#time-list").html(data);
+              
+            }
+        });
+    }
+
+</script>
+
 <body>
-
-        <div id="box">
-            <form method="post">
-            <div style="font-size: 20px; margin: 10px; color: white;">first</div>
-
-            <p>Please fill out the needed information:</p>
-
-            <!--<label for="passengers">How many passengers: </label>
-            <input id="text" type="text" name="passengers"> <br><br>-->
-
-            <label for="passengertype">Type of passenger:</label>
-            <select name="passengertype" id="passengertype">
-                <option value="regular">Regular</option>
-                <option value="student">Student</option>
-                <option value="pregnant">Pregnant</option>
-                <option value="pwd">PWD</option>
-                <option value="senior">Senior</option>
+    <div class="frm">
+        <h2>Please complete all the informations needed</h2>
+        <div class="row">
+            <label>Bound:</label><br>
+            <select name="bound" id="bound-list" class="InputBox" onChange="getDeparture(this.value);">
+                <option value disabled selected>Select Bound</option>
+                <?php
+                    foreach($results as $bound) {
+                ?>
+                <option value = "<?php echo $bound["id"]; ?>"><?php echo $bound["bound"]; ?></option>
+                <?php
+                    }
+                ?>
             </select>
-                <br><br>
-
-            <label for="departure">Departure Station: </label> 
-            <select name="departure" id="departure"> 
-                <option value="alabang">Alabang</option>
-                <option value="sucat">Sucat</option>
-                <option value="bicutan">Bicutan</option>
-                <option value="fti">FTI</option>
-                <option value="nichols">Nichols</option>
-                <option value="edsa">EDSA</option>
-                <option value="pasay">Pasay</option>
-                <option value="buendia">Buendia</option>
-                <option value="vitocruz">Vito Cruz</option>
-                <option value="sanandres">San Andres</option>
-                <option value="paco">Paco</option>
-                <option value="pandacan">Pandacan</option>
-                <option value="stamesa">Sta. Mesa</option>
-                <option value="espana">España</option>
-                <option value="laonlaan">Laon-laan</option>
-                <option value="blumentritt">Blumentritt</option>
-                <option value="tutuban">Tutuban</option>
+        </div>
+        <div class="row">
+            <label>Departure Station:</label><br>
+            <select name="departure" id="departure-list" class="InputBox" onChange="getArrival(this.value);">
+                <option value="">Select Departure Station</option>
             </select>
-                <br><br>
-
-            <label for="arrival">Arrival Station: </label> 
-            <select name="arrival" id="arrival"> 
-                <option value="alabang">Alabang</option>
-                <option value="sucat">Sucat</option>
-                <option value="bicutan">Bicutan</option>
-                <option value="fti">FTI</option>
-                <option value="nichols">Nichols</option>
-                <option value="edsa">EDSA</option>
-                <option value="pasay">Pasay</option>
-                <option value="buendia">Buendia</option>
-                <option value="vitocruz">Vito Cruz</option>
-                <option value="sanandres">San Andres</option>
-                <option value="paco">Paco</option>
-                <option value="pandacan">Pandacan</option>
-                <option value="stamesa">Sta. Mesa</option>
-                <option value="espana">España</option>
-                <option value="laonlaan">Laon-laan</option>
-                <option value="blumentritt">Blumentritt</option>
-                <option value="tutuban">Tutuban</option>
+        </div>
+        <div class="row">
+            <label>Arrival Station:</label><br>
+            <select name="arrival" id="arrival-list" class="InputBox" onChange="getTime(this.value);">
+                <option value="">Select Arrival Station</option>
             </select>
-            <br><br>
-                
-            <input id="button" type="submit" value="proceed"> 
-
-                
-            </form>
+        </div>
+        <div class="row">
+            <label>Time:</label><br>
+            <select name="time" id="time-list" class="InputBox">
+                <option value="">Select Time</option>
+            </select>
         </div>
 
+    </div>
     
 </body>
-</head>
 </html>
