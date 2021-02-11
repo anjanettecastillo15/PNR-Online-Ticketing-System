7a7821b77944f1fe7b8b1ce5f4ced81027b54ef7
gilliancarballo <gillian.carballo0319@gmail.com>
2021-02-11 10:25:07 +0800

    feb 11 gill commit

---
+++ index2.php
@@ -0,0 +1,42 @@
+<?php 
+
+session_start();
+
+include("connection.php");
+include("functions.php");
+?>
+
+<html>
+<style>
+#box{
+            background: rgb(240,248,255,0.7);
+            margin: auto;
+            text-align:center;
+            margin-top: 150px;
+            margin-bottom: 150px;
+            width: 300px;
+            padding: 20px;
+}
+#title{
+            background: #FF8C00;
+            opacity:0.7;
+            text-align:center;
+            font-family: sans-serif;
+            color: black;
+            padding: 2px;
+           
+        }
+
+</style>
+<body  style='background: url(train.jpg); background-repeat:no-repeat; background-size:100% 100%; background-size: cover;'> 
+<div id="title"> <h1>PNR Online Ticketing System<h1> </div>
+    <div id="box">
+        <form method="post">
+        <div style="font-size: 30px; margin: 10px; color: black; font-weight:bold;">PAYMENT</div>
+
+            <input id="text" type="text" name="user_name"  placeholder="Enter amount"> <br><br>
+            <input id="button" type="submit" value="PAY"> <br><br>
+        </form>
+    </div>
+</body>
+</html>
