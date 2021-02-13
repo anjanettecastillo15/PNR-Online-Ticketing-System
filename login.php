05ba4eca142b6bf4b3bbceeb77143d29199b7947
gilliancarballo <gillian.carballo0319@gmail.com>
2021-02-13 19:00:56 +0800

    feb 13 gill commit

--- login.php
+++ login.php
@@ -54,6 +54,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
    
     <h1><span> PNR ONLINE TICKETING SYSTEM</span></h1>
     <nav>
+
         <ul>
             <li><a href="signup.php">Sign up</a><li>
         </ul>
@@ -61,27 +62,26 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 </header> 
     <style type="text/css">
-        #text{
+        /* #text{
             height: 25px;
             border-radius: 5px;
             padding: 4px;
             border: solid thin #aaa;
             width: 100%;
-        }
+        } */
 
-        #button{
+        /* #button{
             margin:auto;
             padding: 10px;
             width: 100px;
             color: white;
             background-color: blue;
             border: none;
-        }
+        } */
 
         #box{
-            background: rgb(240,248,255,0.7);
+            background: rgb(240,248,255,0.8);
             margin: auto;
-            text-align:center;
             margin-top: 150px;
             margin-bottom: 150px;
             width: 300px;
@@ -90,24 +90,70 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
         #title{
             background: #FF8C00;
-            opacity:0.7;
+            opacity:0.4;
             text-align:center;
             font-family: sans-serif;
             color: black;
             padding: 2px;
            
         }
+        #log{
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            position:absolute;
+            top:19%;
+            left: 47%;        
+    
+        }
+        #box p{
+            margin:0;
+            padding: 0;
+            font-weight: bold;
+        }
+        #box input{
+            width:100%;
+            margin-bottom: 20px;
+        }
+        #box input[type="text"], input[type="password"]
+        {
+            border:none;
+            border-bottom: 1px solid black;
+            background: transparent;
+            outline:none;
+            height: 40px;
+        }
+        #box input[type="submit"]
+        {
+            border: none;
+            outline: none;
+            height: 40px;
+            background: #1E90FF;
+            color: #fff;
+            font-size: 18px;
+            border-radius: 20px; 
+
+        }
+
+        #box input[type= "submit"]:hover
+        {
+            cursor:pointer;
+            background: #FFA500;
+            color: #000;
+        }
     </style>
 
    
     <div id="box">
+    <img src="log.jpg" id="log">
         <form method="post">
-        <div style="font-size: 30px; margin: 10px; color: black; font-weight:bold;">Log in</div>
+        <div style="font-size: 30px; margin: 10px; color: black; font-weight:bold;text-align:center">Log in</div>
+            <p>Username</p>
+            <input type="text" name="user_name"  placeholder="Enter your username">
+            <p>Password</p>
+            <input type="password" name="password" placeholder="Enter your password">
 
-            <input id="text" type="text" name="user_name"  placeholder="Enter username"> <br><br>
-            <input id="text" type="password" name="password" placeholder="Enter password"> <br><br>
-
-            <input id="button" type="submit" value="Log in"> <br><br>
+            <input type="submit" value="Log in">
         </form>
     </div>
 </body>
