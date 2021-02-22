c92050f7d8a02920f205b5433e4f9f45aad2f89b
anjanettecastillo15 <adventureswithanjanette@gmail.com>
2021-02-22 12:20:07 +0800

    anja commit changed user_id to id sa loob ng session feb 22

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
 
-    if(isset($_SESSION['user_id'])){
+    if(isset($_SESSION['id'])){
         
-        unset($_SESSION['user_id']);
+        unset($_SESSION['id']);
     }
 
     header("Location: login.php");
